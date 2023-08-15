3c21bc8cb01a22097ef8d89dfaf439cc01704be2
abdallahmstoohy <abdallahforbusiness@gmail.com>
2023-08-15 22:20:45 +0300

    done

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - this is a very fine main
+ * wanna taste it?
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i >= 97; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
