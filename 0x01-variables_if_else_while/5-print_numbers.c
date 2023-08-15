208c02760ddbc70015d57c7d83b6b5fd7d5787b2
abdallahmstoohy <abdallahforbusiness@gmail.com>
2023-08-15 22:05:39 +0300

    done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - this is the main baby
+ * is this the main? yeah this is the main
+ * Return: 0
+ */
+int i;
+
+int main(void)
+{
+	for (i = 0; i < 10; i++)
+		printf("%d\n", n);
+	return (0);
+}
