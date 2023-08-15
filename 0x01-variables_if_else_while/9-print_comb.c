efbf73b488a92c0c567d9a282472f76bc6cde47e
abdallahmstoohy <abdallahforbusiness@gmail.com>
2023-08-15 22:37:44 +0300

    done

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - this is a weird mommy main today
+ * let's see hhahahahhohoohehehmocasho
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		puthcar(44);
+		putchar(32);
+	}
+	putchar('\n');
+	return (0);
+}
