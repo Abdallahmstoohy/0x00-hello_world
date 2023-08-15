a868139356964698b44f1c08daa9b915b212722a
abdallahmstoohy <abdallahforbusiness@gmail.com>
2023-08-15 22:13:56 +0300

    done

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - heke mikey
+ * trabantiky feel sadiky
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i);
+	putchar('\n')
+}
