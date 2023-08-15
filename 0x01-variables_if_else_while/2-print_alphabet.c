e3dac5e9a56f940f5af757527eac1aa3d7c44ae2
abdallahmstoohy <abdallahforbusiness@gmail.com>
2023-08-15 12:42:01 +0300

    done

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
  * main - printing alphabit
- * ohmy god 
+ * ohmy god
  * Return: 0
  */
 void main(void)
 {
 	int i;
-	for (i=97;i<=122;i++)
+
+	for (i = 97; i <= 122; i++)
 		putchar(i)
 }
