098f848d42d99909f4988e2256219692b8e85c83
abdallahmstoohy <abdallahforbusiness@gmail.com>
2023-08-15 22:30:54 +0300

    done

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - i hate this main tbh
+ * let's see
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	int c;
+
+	for (i = 48; i < 58; i++)
+		putchar(i);
+	for (c = 97; c < 103; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
