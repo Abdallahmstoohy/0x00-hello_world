f4eaeccad5eabfc72c181efaf20a5b56a5fc1b2a
Abdallahmstoohy <abdallahforbusiness@gmail.com>
2023-10-05 18:00:22 +0300

    hi

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+/**
+*main-causesaninfiniteloop
+*Return:0
+*/
+
+intmain(void)
+{
+	inti;
+
+	printf("Infiniteloopincoming:(\n");
+
+	i = 0;
+
+/**
+ * while(i<10)
+ *{
+ *putchar(i);
+ *}
+*/
+
+	printf("Infiniteloopavoided!\\o/\n");
+
+	return (0);
+}
