0b45db295cec4866c5aa8881060e77a119e3172a
Abdallahmstoohy <abdallahforbusiness@gmail.com>
2023-10-05 15:27:45 +0300

    hi

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+ * print_times_table - entry point
+ * @n: no baby
+ * Return: nothing
+ */
+void print_times_table(int n)
+{
+	if ((n < 15) || (n < 0))
+	{
+		int i, j, r;
+
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('0');
+			_putchar(',');
+			for (j = 1; j <= n; j++)
+			{
+				r= i * j;
+				if (r > 99)
+				{
+					_putchar((r/ 100) + '0');
+					_putchar(((r / 10) % 10) + '0');
+					_putchar((r % 10) + '0');
+				}
+				else if ((r > 9) && (r < 100))
+				{
+					_putchar(' ');
+					_putchar((r / 10) + '0');
+					putchar((r % 10) + '0');
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar(' ');
+					putchar(r + '0');
+				}
+				if (j != n)
+				{
+					_putchar(r + '0');
+					_putchar(',');
+				}
+			}
+		}
+	}
+}
+					
